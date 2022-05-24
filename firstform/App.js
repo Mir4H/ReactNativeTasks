e0f3b69a41f8a7df2bafc4ea45462cb18685c2b0
MiraHämäläinen <mira.m.hamalainen@gmail.com>
2022-05-24 09:09:49 +0300

    styling

--- firstform/App.js
+++ firstform/App.js
@@ -1,53 +1,47 @@
 import React, {useState} from 'react';
 import type {Node} from 'react';
-import {Button, StyleSheet, Text, TextInput, View,} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const App = () => {
-  const [boot, setBoot]=useState();
   return (
-        <View style={styles.container}>
-          <View style={styles.mainform}>
-          <View style={styles.formstyle}>
-          <TextInput style={styles.textinput}/>
-          <Button color='#38b058' title='OK'/>
-          </View>
-          <View style={styles.formstyle}>
-          <TextInput style={styles.textinput}/>
-          <Button color='gray' title='Cancel'/>
-          </View>
-          </View>
+    <View style={styles.container}>
+      <View style={styles.formstyle}>
+        <TextInput style={styles.textinput} />
+        <TextInput style={styles.textinput} />
+      </View>
+      <View style={styles.formstyle}>
+        <View style={styles.buttonstyle}>
+          <Button color="#38b058" title="OK" />
         </View>
+        <View style={styles.buttonstyle}>
+          <Button color="gray" title="Cancel" />
+        </View>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
-  }, 
-  text:{
-    fontSize: 24,
-    padding: 5,
-    fontWeight:'bold',
+  container: {
+    flex: 1,
+    alignItems: 'center',
   },
-  mainform: {
-    alignItems: 'flex-start',
+  textinput: {
+    width: '40%',
+    backgroundColor: '#d6d6d6',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  buttonstyle: {
+    width: '40%',
+  },
+  formstyle: {
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
   },
-  textinput:{
-    backgroundColor:'#d6d6d6',
-    borderColor:'gray',
-    borderWidth:1,
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  formstyle:{
-    width:'40%',
-    flexDirection:'column',
-    justifyContent:'space-around',
-  }
 });
 
 export default App;
